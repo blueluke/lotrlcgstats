d3d501926d51d66133aedb2432edd9a234e2c477
Lukas Geis <mail.blueluke@yahoo.de>
2016-03-16 17:13:05 +0100

    QuestParse: Enemy and location stats, csv output

--- QuestsParse.py
+++ QuestsParse.py
@@ -10,20 +10,42 @@ import json
 with open('Dictionary.json') as infile:    
     allcards = json.load(infile)
 
+# CSV output
+csvfile = open('stats.csv', 'w')
+# Edit 'fieldnames' to change output values
+# fieldnames = ["QuestName", "EnemyTotal", "EnemyThreatTotal", "EnemyAttackTotal", "EnemyDefenseTotal", "EnemyHealthTotal", "EnemyThreatAverage", "EnemyAttackAverage", "EnemyDefenseAverage", "EnemyHealthAverage", "LocationTotal", "LocationQuestPointsTotal", "LocationThreatTotal", "LocationThreatAverage", "LocationQuestPointsAverage"]
+fieldnames = ["QuestName", "EnemyTotal", "EnemyThreatTotal", "EnemyAttackTotal", "EnemyDefenseTotal", "EnemyHealthTotal"]
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+writer.writeheader() # comment this if you don't want a header row
+
 # Loop over quests
-directories = ['Easy','Quests','Nightmare']
+#directories = ['Easy','Quests','Nightmare']
+directories = ['Quests']
 for directory in directories:
-    for o8dfile in os.listdir(directory):
+    for o8dfile in sorted(os.listdir(directory)):
         tree = ET.parse(directory+'/'+o8dfile)
         root = tree.getroot()
         stats = {
+            "QuestName": "",
             "EnemyTotal": 0,
-            "EnemyThreatTotal": 0
+            "EnemyThreatTotal": 0,
+            "EnemyAttackTotal": 0,
+            "EnemyDefenseTotal": 0,
+            "EnemyHealthTotal": 0,
+            "EnemyThreatAverage": 0,
+            "EnemyAttackAverage": 0,
+            "EnemyDefenseAverage": 0,
+            "EnemyHealthAverage": 0,
+            "LocationTotal": 0,
+            "LocationQuestPointsTotal": 0,
+            "LocationThreatTotal": 0
         }
+        stats["QuestName"] = o8dfile[10:-4]
         for card in root.getiterator('card'):
             OctgnID = card.get('id',0)
             Quantity = int(card.get('qty',0))
             cardObject = allcards.get(OctgnID,{});
+            # Enemies
             if (cardObject.get("Type","") == "Enemy"):
                 stats["EnemyTotal"] = stats["EnemyTotal"] + Quantity
                 # Calculate threat
@@ -31,13 +53,65 @@ for directory in directories:
                 if (Threat.isdigit()): 
                     Threat = int(Threat)
                 else:  # in case Threat == 'X' or '-'
-                    Threat = 0;
-                
+                    Threat = 0;                
                 stats["EnemyThreatTotal"] = stats["EnemyThreatTotal"] + Threat*Quantity
+                # Calculate Attack
+                Attack = cardObject.get("Attack",0)
+                if (Attack.isdigit()): 
+                    Attack = int(Attack)
+                else:  # in case Attack == 'X' or '-'
+                    Attack = 0;                
+                stats["EnemyAttackTotal"] = stats["EnemyAttackTotal"] + Attack*Quantity
+                # Calculate Defense
+                Defense = cardObject.get("Defense",0)
+                if (Defense.isdigit()): 
+                    Defense = int(Defense)
+                else:  # in case Defense == 'X' or '-'
+                    Defense = 0;                
+                stats["EnemyDefenseTotal"] = stats["EnemyDefenseTotal"] + Defense*Quantity
+                                # Calculate Health
+                Health = cardObject.get("Health",0)
+                if (Health.isdigit()): 
+                    Health = int(Health)
+                else:  # in case Health == 'X' or '-'
+                    Health = 0;                
+                stats["EnemyHealthTotal"] = stats["EnemyHealthTotal"] + Health*Quantity
+
+            # Locations
+            if (cardObject.get("Type","") == "Location"):
+                stats["LocationTotal"] = stats["LocationTotal"] + Quantity
+                # Calculate quest points
+                QuestPoints = cardObject.get("Quest Points",0)
+                if (QuestPoints.isdigit()): 
+                    QuestPoints = int(QuestPoints)
+                else:  # in case QP == 'X' or '-'
+                    QuestPoints = 0;                    
+                stats["LocationQuestPointsTotal"] = stats["LocationQuestPointsTotal"] + QuestPoints*Quantity
+                # Calculate threat
+                Threat = cardObject.get("Threat",0)
+                if (Threat.isdigit()): 
+                    Threat = int(Threat)
+                else:  # in case Threat == 'X' or '-'
+                    Threat = 0;                
+                stats["LocationThreatTotal"] = stats["LocationThreatTotal"] + Threat*Quantity
+
         
         # Calculate averages
         stats["EnemyThreatAverage"] = float(stats["EnemyThreatTotal"])/stats["EnemyTotal"]
-        # Print
-        print o8dfile
-        print stats
+        stats["EnemyAttackAverage"] = float(stats["EnemyAttackTotal"])/stats["EnemyTotal"]
+        stats["EnemyDefenseAverage"] = float(stats["EnemyDefenseTotal"])/stats["EnemyTotal"]
+        stats["EnemyHealthAverage"] = float(stats["EnemyHealthTotal"])/stats["EnemyTotal"]
+        stats["LocationQuestPointsAverage"] = float(stats["LocationQuestPointsTotal"])/stats["LocationTotal"]
+        stats["LocationThreatAverage"] = float(stats["LocationThreatTotal"])/stats["LocationTotal"]
+
+        # Print to stout - uncomment this if you want console output
+        # print o8dfile
+        # print stats
+
+        # Write row to csvfile
+        writer.writerow(stats)
+
+
+
+
 
